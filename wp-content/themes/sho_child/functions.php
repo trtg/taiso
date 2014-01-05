665a27642d3ca7c6f53c4bd66d62d7e78191be7d
Sebastian Ortiz <sebortiz@gmail.com>
2014-01-04 18:06:16 -0800

    created apparatus, level, and function taxonomies that get assigned to the movement custom post type

--- wp-content/themes/sho_child/functions.php
+++ wp-content/themes/sho_child/functions.php
@@ -1,15 +1,95 @@
 <?php
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
-    register_post_type( 'gymnastic_skill',
+    register_post_type( 'movement',
         array(
             'labels' => array(
-                'name' => __( 'Skills' ),
-                'singular_name' => __( 'Skill' )
+                'name' => __( 'Movements' ),
+                'singular_name' => __( 'Movement' )
             ),
-        'public' => true,
-        'has_archive' => true,
+            'public' => true,
+            'has_archive' => true,
         )
     );
 }
+
+add_action('init', 'cptui_register_my_taxonomies');
+function cptui_register_my_taxonomies() {
+    register_taxonomy( 'level',array (
+        0 => 'movement',
+    ),
+    array( 'hierarchical' => false,
+    'label' => 'Level',
+    'show_ui' => true,
+    'query_var' => true,
+    'show_admin_column' => false,
+    'labels' => array (
+        'search_items' => 'Level',
+        'popular_items' => '',
+        'all_items' => '',
+        'parent_item' => '',
+        'parent_item_colon' => '',
+        'edit_item' => '',
+        'update_item' => '',
+        'add_new_item' => '',
+        'new_item_name' => '',
+        'separate_items_with_commas' => '',
+        'add_or_remove_items' => '',
+        'choose_from_most_used' => '',
+    )
+) );
+
+    register_taxonomy( 'apparatus',array (
+        0 => 'movement',
+    ),
+    array( 'hierarchical' => false,
+    'label' => 'Apparatus',
+    'show_ui' => true,
+    'query_var' => true,
+    'show_admin_column' => false,
+    'labels' => array (
+        'search_items' => 'Apparatus',
+        'popular_items' => '',
+        'all_items' => '',
+        'parent_item' => '',
+        'parent_item_colon' => '',
+        'edit_item' => '',
+        'update_item' => '',
+        'add_new_item' => '',
+        'new_item_name' => '',
+        'separate_items_with_commas' => '',
+        'add_or_remove_items' => '',
+        'choose_from_most_used' => '',
+    )
+) );
+
+    register_taxonomy( 'function',array (
+        0 => 'movement',
+    ),
+    array( 'hierarchical' => false,
+    'label' => 'Function',
+    'show_ui' => true,
+    'query_var' => true,
+    'show_admin_column' => false,
+    'labels' => array (
+        'search_items' => 'Function',
+        'popular_items' => '',
+        'all_items' => '',
+        'parent_item' => '',
+        'parent_item_colon' => '',
+        'edit_item' => '',
+        'update_item' => '',
+        'add_new_item' => '',
+        'new_item_name' => '',
+        'separate_items_with_commas' => '',
+        'add_or_remove_items' => '',
+        'choose_from_most_used' => '',
+    )
+) ); 
+
+
+}
+
+
+
 ?>
