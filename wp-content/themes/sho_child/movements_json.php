adcffd429723f6f7282a446e238d4d9e88ffaae4
Sebastian Ortiz <sebortiz@gmail.com>
2014-02-16 21:32:57 -0800

    first pass at exposing endpoint for movements to be used by the directory

---
+++ wp-content/themes/sho_child/movements_json.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: movements_json
+ * This is an endpoint which exposes all custom posts of 
+ * type movement as a JSON list.
+ *
+ * @package taiso
+ */
+
+?>
+
+<?php 
+while ( have_posts() ) : the_post();
+endwhile;
+?>
+
+<?php 
+
+$movements = new WP_Query(array(
+    'post_type' => 'movement'
+));
+
+$list_of_movements = array();
+while($movements->have_posts()) : $movements->the_post();
+array_push($list_of_movements,the_post());
+endwhile;
+echo json_encode($list_of_movements);
+?>
