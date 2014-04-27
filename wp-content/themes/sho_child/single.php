500779f04f5ad92d752d040ad272eee0f11742de
Sebastian Ortiz <sebortiz@gmail.com>
2014-04-27 12:55:49 -0700

    wrapping blog posts into fluid grid

--- wp-content/themes/sho_child/single.php
+++ wp-content/themes/sho_child/single.php
@@ -11,19 +11,23 @@ get_header(); ?>
 <body>
 <?php include 'navbar.php';?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+	<div id="wrapper" class="container">
 			<?php
 				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
+                while ( have_posts() ) : the_post();?>
+                    <div class="row">
+                        <div class="col-xs-12">
+                    <?php
 					/*
 					 * Include the post format-specific template for the content. If you want to
 					 * use this in a child theme, then include a file called called content-___.php
 					 * (where ___ is the post format) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+                             get_template_part( 'content', get_post_format() );?>
+                        </div><!--/col-->
+                    </div>
 
+                    <?php
 					// Previous/next post navigation.
 					twentyfourteen_post_nav();
 
@@ -33,8 +37,7 @@ get_header(); ?>
 					}
 				endwhile;
 			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
+	</div><!-- #container -->
 </body>
 <?php include 'footer.php';?>
 <?php //get_sidebar( 'content' ); get_sidebar(); get_footer(); ?>
