18d2bcaaddc3c111069b8365f8ae0913c865691e
Sebastian Ortiz <sebortiz@gmail.com>
2014-06-26 19:09:56 -0700

    include the post title in the JSON for display purposes in the movement directory

--- wp-content/themes/sho_child/movements_json.php
+++ wp-content/themes/sho_child/movements_json.php
@@ -33,6 +33,7 @@ foreach ($movement_posts as $movement_post){
     $url = wp_get_attachment_image_src( get_post_thumbnail_id($movement_post->ID) );
     $post_data['thumbnail_url'] = $url;
     $post_data['name'] = $movement_post->post_name;
+    $post_data['title'] = $movement_post->post_title;
     $post_data['permalink'] = get_post_permalink($movement_post->ID);
     $apparatus_terms = get_the_terms($movement_post->ID,'apparatus');
     $post_data['apparatus'] = array();
