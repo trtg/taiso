0c2a578855f6ac1ec2e858ccc6f0647faa86ba5c
Sebastian Ortiz <sebortiz@gmail.com>
2014-08-27 15:43:03 -0700

    adding the prettier template for about taiso, next need to fix up all the file paths and upload images

---
+++ wp-content/themes/sho_child/about_taiso_updated.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * Template Name: about_taiso_updated
+ * The template for the about taiso page 
+ *
+ * @package taiso
+ */
+
+get_header(); ?>
+    <title>Total Taiso</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width; initial-scale=1; minimal-ui">
+
+    <meta name="author" content="Sho Nakomori">
+    <meta name="description" content="Total Taiso desc" />
+    <meta name="author" content="TotalTaiso  Campaign">
+    <meta name="keywords" content="strength, balance, flexibility ....">
+    
+    <!-- Bootstrap core CSS -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bannerscollection_zoominout.css" rel="stylesheet">
+    
+    <script src="js/jquery1.8.3.js" type="text/javascript"></script>  
+    <script src="js/jquery-ui.min.js"></script>
+  
+  
+
+    <!-- support till IE8 and a bit 7 -->
+
+
+    <!--[if lt IE 9]>
+        <link href="font-awesome/css/font-awesome-ie7.min.css" rel="stylesheet">
+         <script src="js/html5shiv.js" type="text/javascript"></script>
+         <script src="js/respond.src.js" type="text/javascript"></script>
+     <![endif]-->
+
+    <!--[if lt IE 8]>
+    <link href="css/bootstrap-ie7.css" rel="stylesheet">
+    <![endif]-->
+
+    
+
+    <!-- Add custom CSS here -->
+
+
+    <link href="main.css" rel="stylesheet">
+    <link href="css/jquery.bxslider.css" rel="stylesheet" />
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+<section id="aboutTaiso">
+
+ <h1>About TotalTaiso</h1>
+ <article class="text-left info">    
+
+ <p class="indent">
+TotalTaiso was built upon the <b>principles of gymnastics</b>, a sport that combines the elements of <b>strength, balance, and flexibility</b> to lead a healthier lifestyle. While the direct translation for the Japanese word, ‘Taiso,’ is gymnastics, it’s more than the <b>type of gymnastics</b> you see on TV. More broadly, <b>‘Taiso’</b> refers to the <b>act of manipulating the body</b>, and using the body as an apparatus for achieving fitness.</p>
+</article>
+<div class="clear"></div>   
+ 
+ <ul class="portfolio-wrap2">
+                  
+             <li class="portfolio-box  photos">    
+                           <img  src="img/aboittaiso/strength.jpg" alt="STRENGTH" />  
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                  <!-- <h5>STRENGTH <span></span></h5>  -->
+                                    <p>STRENGTH</p>
+                                </figcaption>           
+                            </figure>
+                               </a>  
+                    </li>       
+                   
+                    <li class="portfolio-box photos ">  
+                       <img  src="img/aboittaiso/sho-nakomori-balance.jpg" alt="balance" />
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                    <!--  <h5>BALANCE <span></span></h5>  -->
+                                    <p> BALANCE</p>
+                                </figcaption>           
+                            </figure>
+                         </a>                 
+                    </li>   
+                   
+                    <li class="portfolio-box  photos">    
+                       <img  src="img/aboittaiso/flexibility.jpg" alt="FLEXIBILITY" />
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                <!-- <h5>FLEXIBILITY <span></span></h5> -->
+                                    <p>FLEXIBILITY</p>
+                                </figcaption>           
+                            </figure>
+                         </a>           
+                    </li>   
+                   
+                    
+                </ul>   
+
+<div class="clear"></div>   
+
+<article class="text-left info">    
+<p class="indent"> We, at <b>TotalTaiso</b>, believe that a fancy gym is not always necessary to <b>achieve fitness,</b> and that all you simply need is your body. Whether you are a hardcore CrossFitter, weekend warrior, or someone that is trying to maintain their new years resolution, we hope that TotalTaiso has the resources necessary to take you to the <b>next level.</b>
+</p>
+</article>
+
+
+ 
+</section>
+
+<!-- end  about taiso -->
+
+
+ <!-- Footer -->
+    <footer>
+        <div class="container no-padding">
+            
+            <a id="back-top"><div id="menu_top"><div id="menu_top_inside"></div></div></a>
+            
+        </div>
+    </footer>
+    <!--/Footer -->
+
+ 
+
+
+        <!-- changed in variables.less @grid-float-breakpoint: 10px; //@screen-sm-min; @screen-phone; -->
+        
+        <footer id="contactfooter">
+            <div class="navbar navbar-inverse navbar-fixed-bottom ">
+                <div class="container">
+                    <div class=" navbar-collapse collapse" id="footer-body">
+                        <ul class="nav navbar-nav">
+                            
+            <div class="sixteen columns icons-footer">
+                <a href="#"><img src="img/social-icons/facebook.png" alt="" /></a>
+                <a href="#"><img src="img/social-icons/twitter.png" alt="" /></a>
+                <a href="#"><img src="img/social-icons/google.png" alt="" /></a>
+                <a href="#"><img src="img/social-icons/mail.png" alt="" /></a>
+                <a href="#"><img src="img/social-icons/youtube.png" alt="" /></a>
+                
+            </div>  
+                           
+
+                    </div>
+
+
+                </div>
+            </div>
+        </footer>
+
+
+
+
+        <!-- JavaScript -->
+        
+        <script src="js/bootstrap.js"></script>
+        <script src="js/jquery.flexslider-min.js"></script> 
+        <script src="js/isotope.js"></script>
+        <script src="js/jquery.bxslider.min.js"></script>
+        <script src="js/main.js"></script>
+        
+         
+
+
+
+<!--  google analitics  -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'here_needs-tobe_tracking_code', 'totaltaiso.com');
+  ga('send', 'pageview');
+
+</script>
+
+
+</body>
+</html>
+
+
