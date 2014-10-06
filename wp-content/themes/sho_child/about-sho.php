59568d191aef825b865cabe6058f8b97ea855168
Sebastian Ortiz <sebortiz@gmail.com>
2014-10-05 21:52:52 -0700

    starting to integrate sergey's version of the about sho page

---
+++ wp-content/themes/sho_child/about-sho.php
@@ -0,0 +1,362 @@
+<?php
+/**
+ * Template Name: about-sho
+ * The template for the about page with biographic info 
+ *
+ * @package taiso
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <title>Total Taiso</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width; initial-scale=1; minimal-ui">
+
+    <meta name="author" content="Sho Nakomori">
+    <meta name="description" content="Total Taiso desc" />
+    <meta name="author" content="TotalTaiso  Campaign">
+    <meta name="keywords" content="strength, balance, flexibility ....">
+    
+    <!-- Bootstrap core CSS -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+   <!--  <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/fancymenu.css"  rel="stylesheet">  -->
+    
+    
+
+    <!-- <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/jquery1.8.3.js" type="text/javascript"></script>  
+    
+    <script src="js/jquery-ui.min.js"></script> -->
+   <!--  <script src="weljs/jquery.ui.touch-punch.min.js" type="text/javascript"></script> -->
+   <!--  <script src="js/bannerscollection_zoominout.js" type="text/javascript"></script> -->
+
+    <!-- support till IE8 and a bit 7 -->
+
+
+    <!--[if lt IE 9]>
+        <link href="font-awesome/css/font-awesome-ie7.min.css" rel="stylesheet">
+         <script src="js/html5shiv.js" type="text/javascript"></script>
+         <script src="js/respond.src.js" type="text/javascript"></script>
+         <link href="css/border-radius.htc" rel="stylesheet">
+     <![endif]-->
+
+    <!--[if lt IE 8]>
+    <link href="css/bootstrap-ie7.css" rel="stylesheet">
+    <![endif]-->
+
+    <!--[if IE]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection" /><![endif]-->
+
+    <!--[if lt IE 8]>
+   <style> .btn{ behavior: url(css/border-radius.htc); position: relative;} .vert-text{ margin-top:50px;} </style>
+<![endif]-->
+    <!-- Add custom CSS here -->
+
+
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" />   
+    <link href="css/fancymenu.css"  rel="stylesheet"> 
+     <link href="style.css" rel="stylesheet" /> 
+     <link href="aboutsho.css" rel="stylesheet" />
+    
+    
+    
+    
+    
+    <link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen" />    
+    <link href="css/jquery.bxslider.css" rel="stylesheet" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css' />
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+   <!--  <link href="main.css" rel="stylesheet">
+    <link href="css/jquery.bxslider.css" rel="stylesheet" />
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+ -->
+
+
+
+
+
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="75" class="royal_loader">
+
+
+<!-- menu -->
+
+   <?php  include 'menu.php'; ?>
+
+
+
+<!-- end menu -->
+
+
+
+ <div id="flare" class="shoflare">
+                  <div class="pattern-about">  
+                     <div class="vert-text">
+                        <h1 class="">Sho Nakamori</br>Athlete & Coach</h1>
+                      </div>
+                  </div> 
+
+    </div>
+
+           
+
+                <!-- About sho SECTION
+    ================================================== -->
+    
+    <section class="work" id="work">
+    
+        <div class="container-sho">
+        <div class="row">
+        
+
+     <div class="imageWrapper">
+        <img src="images/v.jpg" class="v-sho">     
+
+            <div class="sixteen columns icons-footer" >
+               <a class="" href="#"> 
+                <img src="images/twitter-60x60.png">
+               </a>
+               <a class="" href="#"> 
+                <img src="images/instagram.png">
+               </a>
+
+                <!-- <a class="fa-instagram" href="#"></a>
+               <a href="#">&#xf099;</a> -->
+                
+            </div>  
+    </div>
+
+ <article class="text-left info ">
+                <p class="indent"> Sho was born into a family of gymnasts and grew up flipping around at the gym. By age twelve, he made the <b>US National Team</b> and maintained his spot until his retirement in 2012. During his career, he garnered multiple National Championships and represented the United States in various international competitions including the <b>World Cup, World Championships, and Pan American Games</b>.</p>
+         
+                <p class="indent secondCap">He went on to <b>captain the <span class="stanford">Stanford University</span> gymnastics team</b> to an NCAA Championship, while also capturing <b>9 individual All-American awards</b>. Sho is an internationally-ranked gymnastics judge through the FIG and a <b>NASM-certified fitness trainer</b>. </p>
+            </article>
+    
+  </div> 
+
+            
+        </div>  
+    
+                <div class="clear"></div>
+
+                <div class="portfolio"></div>
+
+
+                <div class="expander-wrap relative">
+                    <div id="expander-wrap">
+                        <p class="cls-btn"><a class="close">X</a></p>
+                        <div class="expander-inner"></div>
+                    </div>
+                </div>
+
+            
+                <!-- <div class="clear"></div> -->
+             
+       <!-- Portfolio Filters -->   
+
+                
+                
+                    <div class="sixteen columns">
+                        <div id="portfolio-filter">
+                            <ul id="filter">
+                                
+                              
+                                <li><a href="#" data-filter=".videos" title="">Videos</a></li>
+                                <li><a href="#" data-filter=".photos" title="">Photos</a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                
+
+
+
+
+                
+                <div class="clear"></div>
+            
+                <ul class="portfolio-wrap">
+                  
+                    <li class="portfolio-box videos ">
+                        <a class="expander" href="aboutsho/high-bar-video.html" title="Visa Championships">
+                            <img  src="images/gallery/high-bar.jpg" alt="" />  
+                            
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                     <h5></h5> 
+                                    <p>2007 Visa Championships - Men<br>High Bar </p>
+                                </figcaption>           
+                            </figure>
+                        </a>            
+                    </li>   
+
+                    <li class="portfolio-box  videos">   
+                        <a class="expander" href="aboutsho/parallel-bars-video.html" title="Pan American Games">
+                            <img  src="images/gallery/pbars-sho-nakomori.jpg" alt="" />
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                     <h5></h5> 
+                                      <p>(USA) PB TF Pan American Games Guadalajara 2011<br>Parallel Bars</p>
+                                </figcaption>           
+                            </figure>
+                        </a>    
+                    </li>
+                     
+                    <li class="portfolio-box  videos">   
+                        <a class="expander" href="aboutsho/adidas-video.html" title="Adidas - All In">
+                            <img  src="images/gallery/adidas.jpg" alt="" />  
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                    <!-- <h5>Sho <span>Nakomory</span></h5> -->
+                                    <p>Adidas - All In Gymnastics<br>Commercial</p>
+                                </figcaption>           
+                            </figure>
+                        </a>            
+                    </li>
+
+                    <li class="portfolio-box videos ">
+                        <a class="expander" href="aboutsho/shoe-video.html" title="Inner youth">
+                            <img  src="images/gallery/shoe2.jpg" alt="" />  
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                   <!--  <h5>Sho <span>Nakomory</span></h5> -->
+                                    <p>Keen Footwear <br>Commercial</p>
+                                </figcaption>           
+                            </figure>
+                        </a>            
+                    </li>   
+
+                    
+                    <li class="portfolio-box  photos">    
+                        <!-- <a class="expander" href="ajax-video-project.html" title=""> -->
+                           <!--  <a title="Pommel Horse" rel="prettyPhoto[gallery]" href="images/gallery/4.jpg">  -->
+                          <a class="expander" href="aboutsho/pommel-slider-gallery.html" title=""> 
+                            <img  src="images/gallery/4.jpg" alt="" />  
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                   <!--  <h5>Sho <span>Nakomory</span></h5> -->
+                                    <p>MPSF Championships (Stanford, CA). <br>Pommel Horse.</p>
+                                </figcaption>           
+                            </figure>
+                               </a>  
+                    </li>       
+                   
+                    <li class="portfolio-box photos ">  
+                       <a class="expander" href="aboutsho/pbars-gallery.html" title=""> 
+                            <img  src="images/gallery/3.jpg" alt="" />
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                    <!-- <h5>Sho <span>Nakomory</span></h5> -->
+                                    <p>  NCAA Championships (Minneapolis, MN). <br>Parallel Bar .</p>
+                                </figcaption>           
+                            </figure>
+                         </a>                 
+                    </li>   
+                   
+                    <li class="portfolio-box  photos">    
+                         <a class="expander" href="aboutsho/still-rings-gallery.html" title="still rings"> 
+                            <img  src="images/gallery/6.jpg" alt="" />
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                   <!--  <h5>Sho <span>Nakomory</span></h5> -->
+                                    <p>MPSF Championships (Stanford, CA). <br>Still Rings .</p>
+                                </figcaption>           
+                            </figure>
+                         </a>           
+                    </li>   
+                    <li class="portfolio-box photos ">
+                         <a class="expander" href="aboutsho/high-bar-gallery.html" title="high-bar"> 
+                            <img  src="images/gallery/7.jpg" alt="" />  
+                            <figure class="effect-sadie">
+                                <figcaption>
+                                   <!--  <h5>Sho <span>Nakomory</span></h5> -->
+                                    <p>NCAA Championships (Minneapolis, MN) . <br>High Bar .</p>
+                                </figcaption>           
+                            </figure>
+                         </a>            
+                    </li>   
+                    
+                </ul>                   
+
+                <div class="clear"></div>   
+
+               <article class="text-left info">
+                   
+                    <p class="indent">He has trained numerous athletes with varying backgrounds - from competitive CrossFitters, yoga instructors, and professional dancers to individuals trying to achieve their fitness goals. Through his coaching experience, Sho developed the ideology for TotalTaiso: all fitness goals can be achieved by attaining the three main components required for gymnastics. These core elements - <b>strength, balance, and flexibility</b> - make up the fundamental principles of TotalTaiso. Sho's hope is to spread his knowledge of gymnastics and body movement to help people reach their personal fitness goals.</p>
+               </article> 
+                
+      
+    
+    </section>
+
+    
+  <!-- Footer -->
+   <?php  include 'footer.php'; ?>
+    <!--/Footer -->
+
+ 
+
+
+      
+        
+
+    
+   <!-- JavaScript -->
+
+
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js'></script>
+    <script src="js/jquery.sticky.js"></script>     
+    <script src="js/jquery.easing-1.3.pack.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="js/jquery.parallax-1.1.3.js" type="text/javascript"></script>
+    <script src="js/appear.js" type="text/javascript" ></script>
+    <script src="js/modernizr.js" type="text/javascript"></script>
+
+    
+    <script type="text/javascript" src="js/royal_preloader.min.js"></script>
+    <script type="text/javascript">
+        (function($) { "use strict";
+                    Royal_Preloader.config({
+                        mode:           'text', // 'number', "text" or "logo"
+                        text:           'ABOUT SHO',
+                        timeout:        0,
+                        showInfo:       true,
+                        opacity:        1,
+                        background:     ['#212121']
+                    });
+        })(jQuery);
+    </script>
+    
+    <script src="js/jquery.prettyPhoto.js" type="text/javascript"></script>
+    <script src="js/isotope.js" type="text/javascript"></script>
+    <script src="js/jquery.bxslider.min.js"></script>
+    <script type="text/javascript" src="js/jquery.tweet.js"></script>
+    <script src="js/jquery.cycle.all.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/jquery.maximage.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/sscr.js"></script>
+    <script src="js/skrollr.js"></script>
+    <script src="js/jquery.jigowatt.js"></script>
+    <script src="js/classie.js" type="text/javascript"></script>
+    <script src="js/fancymenu-rollin.js" type="text/javascript"></script>
+    
+     <script src="js/absho.js" type="text/javascript"></script> 
+
+        
+     
+
+</body>
+
+</html>
---
+++ wp-content/themes/sho_child/menu.php
@@ -0,0 +1,54 @@
+ <div class="overlay overlay-hugeinc">
+            <button type="button" class="overlay-close">Close</button>
+            <nav>
+                <ul>
+                    <li><a class="fancy-nav-close scroll" href="/"><span>TotalTaiso</span></a></li>
+                    <li><a class="fancy-nav-close scroll" href="#about"><span>Movements</span></a></li>    
+                    <li><a class="fancy-nav-close scroll" href="coaching.php"><span>Coaching</span></a></li>
+                    <li><a class="fancy-nav-close scroll" href="blog.php"><span>Blog</span></a></li>
+                    <!-- <li><a class="fancy-nav-close scroll" href="faq.php"><span>Faq</span></a></li>
+                    <li><a class="fancy-nav-close scroll" href="disclaimer.php"><span>Disclaimer</span></a></li> -->
+
+                    <li><a class="fancy-nav-close scroll" href=""><span> </span></a></li>
+                    <li><a class="fancy-nav-close scroll" href="about-sho.php"><span style="text-transform:none; font-size:13px">About Sho</span></a></li>
+                    <li><a class="fancy-nav-close scroll" href="about-taiso.php"><span style="text-transform:none; font-size:13px; line-height:18px;">About Taiso</span></a></li>
+                    <!-- <li><a class="fancy-nav-close scroll" href="#contact"><span>Contact</span></a></li> -->
+                    
+                  
+                </ul>
+
+               <!--  <ul class="about">
+                    
+                </ul> -->
+             <!--  <div id="socialicons-container">  
+                 <ul class="icons-footer">
+                 <li> <a href="#"><img src="images/social-icons/facebook.png" alt="" /></a></li>
+                <li><a href="#"><img src="images/social-icons/twitter.png" alt="" /></a></li>
+                <li><a href="#"><img src="images/social-icons/google.png" alt="" /></a></li>
+                <li><a href="#"><img src="images/social-icons/mail.png" alt="" /></a></li>
+                <li><a href="#"><img src="images/social-icons/youtube.png" alt="" /></a></li>
+                
+              </ul> 
+              </div>  -->
+            
+            </nav>
+        </div>
+
+
+<div class="static-side-navigation-wrap grey-bg">
+                <p><button id="trigger-overlay" type="button"></button></p>
+    <ul class="navigation">
+        <li data-slide="1" ></li>
+        <li data-slide="2"></li>
+        <li data-slide="3"></li>
+        <li data-slide="4"></li>
+                <li data-slide="5"></li>
+                <li data-slide="6"></li>
+                <li data-slide="7"></li>
+             <li data-slide="8"></li>
+    </ul>
+    <div class="vertical-logo">
+        <a href="#"><img title="presence" alt="presence" src="images/vertical-logo.png"/></a>
+    </div>
+
+</div>
