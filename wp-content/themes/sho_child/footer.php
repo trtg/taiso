404b78b18c0df9a5555d898a2e07fee1d6128f7b
Sebastian Ortiz <sebortiz@gmail.com>
2014-10-05 23:04:37 -0700

    fix social media icon path

--- wp-content/themes/sho_child/footer.php
+++ wp-content/themes/sho_child/footer.php
@@ -9,11 +9,11 @@
             <div id="socialicons-container">  
                  
                  <ul class="icons-footer">
-                       <li> <a href="#"><img src="images/social-icons/facebook.png" alt="" /></a></li>
-                       <li><a href="#"><img src="images/social-icons/insta.png" alt="" /></a></li>
-                       <li><a href="#"><img src="images/social-icons/twitter.png" alt="" /></a></li>
-                        <li><a href="#"><img src="images/social-icons/google.png" alt="" /></a></li> 
-                        <li><a href="#"><img src="images/social-icons/youtube.png" alt="" /></a></li> 
+                       <li> <a href="#"><img src="/wp-content/images/social-icons/facebook.png" alt="" /></a></li>
+                       <li><a href="#"><img src="/wp-content/images/social-icons/insta.png" alt="" /></a></li>
+                       <li><a href="#"><img src="/wp-content/images/social-icons/twitter.png" alt="" /></a></li>
+                        <li><a href="#"><img src="/wp-content/images/social-icons/google.png" alt="" /></a></li> 
+                        <li><a href="#"><img src="/wp-content/images/social-icons/youtube.png" alt="" /></a></li> 
                        <li><a href="contact.php"><img src="images/social-icons/mail.png" alt="" /></a></li>
                      <!--   <li>
                         
