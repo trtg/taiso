08cbbb0bb80e8af330a20b3dc7116fa326561d97
Sebastian Ortiz <sebortiz@gmail.com>
2014-10-19 14:52:41 -0700

    trying to fix layout and color issues

--- wp-content/themes/sho_child/js/coaching.js
+++ wp-content/themes/sho_child/js/coaching.js
@@ -1,26 +1,58 @@
-$(document).ready( function() {
-    $('#myCarousel').carousel({
-    	interval:   false
-	});
-	
-	var clickEvent = false;
-	$('#myCarousel').on('click', '.nav a', function() {
-			clickEvent = true;
-			$('.nav li').removeClass('active');
-			$(this).parent().addClass('active');		
-	}).on('slid.bs.carousel', function(e) {
-		if(!clickEvent) {
-			var count =2;  //$('.nav').children().length -1;
-			console.log(count);
-			var current = $('.nav li.active');
-			current.removeClass('active').next().addClass('active');
-			var id = parseInt(current.data('slide-to'));
-			console.log(id);
-			if(count == id) {
-			    
-				$('.nav li').first().addClass('active');	
-			}
-		}
-		clickEvent = false;
-	});
+
+//Back To Top
+$(function() {
+    $('#back-top').click(function() {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+    });
+
+
+
 });
+
+
+
+$(document).ready(function() {
+
+
+/*    $('#myCarousel').carousel({
+        interval: 60000
+    });*/
+
+    var clickEvent = false;
+    $('#myCarousel').on('click', '.nav a', function() {
+        clickEvent = true;
+        $('.nav li').removeClass('active');
+        $(this).parent().addClass('active');
+    }).on('slid.bs.carousel', function(e) {
+        if (!clickEvent) {
+            var count = 2; //$('.nav').children().length -1;
+            //console.log(count);
+            var current = $('.nav li.active');
+            current.removeClass('active').next().addClass('active');
+            var id = parseInt(current.data('slide-to'));
+            //console.log(id);
+            if (count == id) {
+
+                $('.nav li').first().addClass('active');
+            }
+        }
+        clickEvent = false;
+    });
+
+
+
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
