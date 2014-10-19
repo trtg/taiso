1aa2b58d79f53aabbced94d56f7b7360879db510
Sebastian Ortiz <sebortiz@gmail.com>
2014-10-18 23:32:09 -0700

    fixing display of footer so black background extends all the way down to bottom of page and copyright is offset from bottom edge

--- wp-content/themes/sho_child/footer.php
+++ wp-content/themes/sho_child/footer.php
@@ -1,13 +1,9 @@
 <!-- Footer -->
     <footer>
         <div class="container no-padding">
-            
             <a id="back-top"><div id="menu_top"><div id="menu_top_inside"></div></div></a>
-
             <!--  <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div> -->
-
             <div id="socialicons-container">  
-                 
                  <ul class="icons-footer">
                        <li> <a href="#"><img src="/wp-content/images/social-icons/facebook.png" alt="" /></a></li>
                        <li><a href="#"><img src="/wp-content/images/social-icons/insta.png" alt="" /></a></li>
@@ -15,27 +11,17 @@
                         <li><a href="#"><img src="/wp-content/images/social-icons/google.png" alt="" /></a></li> 
                         <li><a href="#"><img src="/wp-content/images/social-icons/youtube.png" alt="" /></a></li> 
                        <li><a href="contact.php"><img src="/wp-content/images/social-icons/mail.png" alt="" /></a></li>
-                     <!--   <li>
-                        
-                       </li> -->
-                 </ul> 
-              
-              </div> 
-
+                </ul> 
+              </div><!--/socialicons-container-->
 
               <div class="disclaimerFaq">
                   <ul>
                     <li><a class="" href="faq.php"><span>Faq</span></a><span> / </span><a class="" href="disclaimer.php"><span>Disclaimer</span></a> </li>
-                    
                   </ul>
-          
-
-              </div>
+              </div><!--/disclaimerFaq-->
 
              <span>&copy;2014 TotalTaiso Crafted in California</span>
-            
-        </div>
-    </footer>
-    <!--/Footer -->
+        </div><!--/container-->
+    </footer> <!--/Footer -->
 
 
