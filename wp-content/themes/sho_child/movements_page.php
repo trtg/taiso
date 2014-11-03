3d50cc6841886a985feb73361a1b52d83682a954
Sebastian Ortiz <sebortiz@gmail.com>
2014-11-02 20:18:18 -0800

    adding some missing css

--- wp-content/themes/sho_child/movements_page.php
+++ wp-content/themes/sho_child/movements_page.php
@@ -9,6 +9,38 @@
  * @package taiso
  */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Total Taiso</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    
+
+    <meta name="author" content="Sho Nakomori">
+    <meta name="description" content="Total Taiso desc" />
+    <meta name="author" content="TotalTaiso  Campaign">
+    <meta name="keywords" content="strength, balance, flexibility ....">
+    
+    <!-- Bootstrap core CSS -->
+    
+
+    <link href="/wp-content/themes/sho_child/css/bootstrap.min.css" rel="stylesheet" /> 
+    <link href="/wp-content/themes/sho_child/css/bootstrap-responsive.min.css" rel="stylesheet" />  
+    <link href="/wp-content/themes/sho_child/css/fancymenu.css"  rel="stylesheet"> 
+     <link href="/wp-content/themes/sho_child/css/style.css" rel="stylesheet" /> 
+    <link href="/wp-content/themes/sho_child/css/coaching.css" rel="stylesheet" />
+    
+    
+    
+     <!-- fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'/>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
 <link rel="stylesheet" type="text/css" href="/wp-content/themes/sho_child/css/movement_directory.css">
 
 <body>
