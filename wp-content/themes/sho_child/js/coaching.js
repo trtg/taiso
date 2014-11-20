34173408220d17d4ed0683c760d8f8399313fb3c
Sebastian Ortiz <sebortiz@gmail.com>
2014-11-20 11:58:15 -0800

    disable automatic transitions

--- wp-content/themes/sho_child/js/coaching.js
+++ wp-content/themes/sho_child/js/coaching.js
@@ -15,39 +15,36 @@ $(function() {
 
 $(document).ready(function() {
 
-
-/*    $('#myCarousel').carousel({
-        interval: 60000
-    });*/
-
     var clickEvent = false;
-$('#myCarousel').on('click','.nav a',function(){
-    var current = $('.nav li.active');
-    var id = parseInt(current.data('slide-to'));
-    $('#myCarousel').carousel(id);
-});
-//    $('#myCarousel').on('click', '.nav a', function() {
+    //disable automatic transitions
+    $('#myCarousel').carousel({interval:false});
 
- /*   $('#myCarousel').on('click', '.nav a', function() {
-        clickEvent = true;
-        $('.nav li').removeClass('active');
-        $(this).parent().addClass('active');
-    }).on('slid.bs.carousel', function(e) {
-        if (!clickEvent) {
-            var count = 2; //$('.nav').children().length -1;
-            //console.log(count);
-            var current = $('.nav li.active');
-            current.removeClass('active').next().addClass('active');
-            var id = parseInt(current.data('slide-to'));
-            //console.log(id);
-            if (count == id) {
-
-                $('.nav li').first().addClass('active');
-            }
-        }
-        clickEvent = false;
+    $('#myCarousel').on('click','.nav a',function(){
+        var current = $('.nav li.active');
+        var id = parseInt(current.data('slide-to'));
+        $('#myCarousel').carousel(id);
     });
-*/
+
+    /*   $('#myCarousel').on('click', '.nav a', function() {
+         clickEvent = true;
+         $('.nav li').removeClass('active');
+         $(this).parent().addClass('active');
+         }).on('slid.bs.carousel', function(e) {
+         if (!clickEvent) {
+         var count = 2; //$('.nav').children().length -1;
+    //console.log(count);
+    var current = $('.nav li.active');
+    current.removeClass('active').next().addClass('active');
+    var id = parseInt(current.data('slide-to'));
+    //console.log(id);
+    if (count == id) {
+
+    $('.nav li').first().addClass('active');
+    }
+    }
+    clickEvent = false;
+    });
+    */
 
 
 }); 
