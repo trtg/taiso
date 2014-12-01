c3b2f9a97071fde87c8b0fa1ddda317d0ac25faa
Sebastian Ortiz <sebortiz@gmail.com>
2014-11-30 22:29:57 -0800

    trying to make apparatus archive use new CSS

--- wp-content/themes/sho_child/taxonomy-apparatus.php
+++ wp-content/themes/sho_child/taxonomy-apparatus.php
@@ -16,8 +16,43 @@
  * @subpackage Twenty_Fourteen
  * @since Twenty Fourteen 1.0
  */
-echo "Using my template";
-get_header(); ?>
+ ?>
+<html lang="en">
+<head>
+    <title>Total Taiso</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    
+
+    <meta name="author" content="Sho Nakomori">
+    <meta name="description" content="Total Taiso desc" />
+    <meta name="author" content="TotalTaiso  Campaign">
+    <meta name="keywords" content="strength, balance, flexibility ....">
+    
+    <!-- Bootstrap core CSS -->
+    
+
+    <link href="/wp-content/themes/sho_child/css/bootstrap.min.css" rel="stylesheet" /> 
+    <link href="/wp-content/themes/sho_child/css/bootstrap-responsive.min.css" rel="stylesheet" />  
+    <link href="/wp-content/themes/sho_child/css/fancymenu.css"  rel="stylesheet"> 
+    <link href="/wp-content/themes/sho_child/css/style.css" rel="stylesheet" /> 
+    <link href="/wp-content/themes/sho_child/css/single-movement.css" rel="stylesheet" /> 
+    
+    
+    
+     <!-- fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'/>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+<!--<link rel="stylesheet" type="text/css" href="/wp-content/themes/sho_child/css/movement_directory.css">-->
+</head>
+<body>
+ <?php  include 'menu.php'; ?>
+
 
 	<section id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
@@ -67,8 +102,18 @@ get_header(); ?>
 			?>
 		</div><!-- #content -->
 	</section><!-- #primary -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!--menu depends on classie and modernizr-->
+         <script src="/wp-content/themes/sho_child/js/classie.js" type="text/javascript"></script>
+         <script src="/wp-content/themes/sho_child/js/modernizr.js" type="text/javascript"></script>
+         <script src="/wp-content/themes/sho_child/js/fancymenu-rollin.js" type="text/javascript"></script>
+
+<?php include 'footer.php';?>
 
 <?php
-get_sidebar( 'content' );
+/*get_sidebar( 'content' );
 get_sidebar();
-get_footer();
+get_footer();*/
+?>
+    </body>
+</html>
