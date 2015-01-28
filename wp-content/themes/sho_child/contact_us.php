395c0c99e4c740062a8ba3f8d6da2fb9ea173b5d
Sebastian Ortiz <sebortiz@gmail.com>
2015-01-27 16:43:31 -0800

    trying to hide the photo column at the right time, so you never end up with a bunch of blank space

--- wp-content/themes/sho_child/contact_us.php
+++ wp-content/themes/sho_child/contact_us.php
@@ -41,7 +41,7 @@
 <div class="container-fluid">
 <div class="row">
 
-<div class="col-md-6 col-xs-12 photo_column visible-lg-*">
+<div class="col-md-6 col-xs-12 photo_column visible-md-block">
 <img class="img-responsive img-thumbnail " src="/wp-content/images/steph_stretching_small.jpg">
 </div>
 
