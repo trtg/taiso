8ee4ac6246095ae872b4d985d758cb2faab868f8
Sebastian Ortiz <sebortiz@gmail.com>
2015-02-06 16:29:18 -0800

    added a theme options page to, at least initially, specify the quote that gets shown on the home page. Later this could contain any other user defined options that affect the theme

--- wp-content/themes/sho_child/functions.php
+++ wp-content/themes/sho_child/functions.php
@@ -1,4 +1,59 @@
 <?php
+
+/*define custom settings here, like the value of the 
+ * quote shown on the homepage*/
+function sho_theme_menu(){
+    add_theme_page( 'Home page quote', 'Theme Options', 'manage_options', 'sho_theme_options.php', 'sho_theme_page');
+}
+add_action('admin_menu','sho_them_menu');
+
+/*display extra text on each section */
+function sho_display_section($section){
+}
+/*display the settings on the page*/
+function sho_display_setting($args){
+extract( $args );
+
+    $option_name = 'sho_theme_options_homepage_quote';
+
+    $options = get_option( $option_name );
+
+    switch ( $type ) {  
+          case 'text':  
+              $options[$id] = stripslashes($options[$id]);  
+              $options[$id] = esc_attr( $options[$id]);  
+              echo "<input class='regular-text$class' type='text' id='$id' name='" . $option_name . "[$id]' value='$options[$id]' />";  
+              echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";  
+          break;  
+    }
+}
+
+function sho_validate_settings($input){
+    return $input;
+}
+/*register settings to use on theme options page*/
+add_action('admin_init', 'sho_register_settings');
+function sho_register_settings(){
+   // Register the settings with Validation callback
+    register_setting( 'sho_theme_options', 'sho_theme_options_homepage_quote', 'sho_validate_settings' );
+
+    // Add settings section
+    add_settings_section( 'sho_text_section', 'Text box Title', 'sho_display_section', 'sho_theme_options.php' );
+
+    // Create textbox field
+    $field_args = array(
+      'type'      => 'text',
+      'id'        => 'sho_textbox',
+      'name'      => 'sho_textbox',
+      'desc'      => 'Homepage quote',
+      'std'       => '',
+      'label_for' => 'sho_textbox',
+      'class'     => 'css_class'
+    );
+
+    add_settings_field( 'homepage_quote_textbox', 'Homepage Quote Textbox', 'sho_display_setting', 'sho_theme_options.php', 'sho_text_section', $field_args ); 
+}
+
 add_action( 'init', 'create_post_types' );
 function create_post_types() {
     register_post_type( 'movement',
---
+++ wp-content/themes/sho_child/sho_theme_options.php
@@ -0,0 +1,19 @@
+function sho_theme_page() {
+?>
+<div class="section panel">
+      <h1>Custom Theme Options</h1>
+      <form method="post" enctype="multipart/form-data" action="options.php">
+        <?php 
+                settings_fields('sho_theme_options'); 
+                    do_settings_sections('sho_theme_options.php');
+                  ?>
+            <p class="submit">  
+                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />  
+            </p>  
+            
+      </form>
+      
+    </div>
+    <?php
+}
+}
