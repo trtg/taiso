1135c5cc23cb9eede42e47463ac266d9c17faaab
sebastian ortiz <sebortiz@gmail.com>
2015-07-13 11:50:41 -0700

    added facebook sharing icon and sharing bar to individual wod pages

--- wp-content/themes/sho_child/single-wod.php
+++ wp-content/themes/sho_child/single-wod.php
@@ -180,6 +180,15 @@ $youtube_thumbnail_url = "http://img.youtube.com/vi/$youtube_video_id/default.jp
                     <div class="col-xs-12 col-md-4">
                     </div>
                 </div>
+ <div class="share"> 
+      <div class="cell"> 
+        <a class="twitter-share-button" href="https://twitter.com/intent/tweet?url=<?php the_permalink();?>" data-size="large"> Tweet</a>
+        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink();?>"><img src=/images/facebook_icon.png" alt="Share on Facebook"></a>
+        <a href="https://pinterest.com/pin/create/button/?url=&media=<?php the_permalink();?>&description=">Pin on Pinterest</a>
+        <a href="https://plus.google.com/share?url=<?php the_permalink();?>">Share on Google+</a>
+    </div>
+</div>
+
 <span class='st_facebook_large' displayText='Facebook'></span>
 <span class='st_twitter_large' displayText='Tweet'></span>
 <span class='st_pinterest_large' displayText='Pinterest'></span>
